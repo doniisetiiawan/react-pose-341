94506d53c4821bb73b586f0903e871e7b91f507f
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-03 11:50:44 +0700

    Refactor animations component

--- src/components/Animations/index.jsx
+++ src/components/Animations/index.jsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import { fadeIn, bounce } from 'react-animations';
-import Radium, { StyleRoot } from 'radium';
+import React, { Component } from 'react';
+import posed from 'react-pose';
+import styled from 'styled-components';
+import './Animations.css';
 
-const styles = {
-  fadeIn: {
-    animation: 'x 1s',
-    animationName: Radium.keyframes(fadeIn, 'fadeIn'),
+const Circle = posed.div({
+  normal: {
+    scale: 1,
   },
-  bounce: {
-    animation: 'x 1s',
-    animationName: Radium.keyframes(bounce, 'bounce'),
+  hover: {
+    scale: 3,
   },
-};
+});
 
-const index = () => (
-  <StyleRoot>
-    <div className="Animations" style={styles.bounce}>
-      <h1>This text will be animated</h1>
-    </div>
-  </StyleRoot>
-);
+const StyledCircle = styled(Circle)`
+  color: white;
+  cursor: pointer;
+  background: blue;
+  line-height: 80px;
+  border-radius: 50%;
+  height: 80px;
+  width: 80px;
+`;
+
+class index extends Component {
+  state = {
+    bg: 'blue',
+    hover: false,
+  };
+
+  handleMouseEnter = () => {
+    this.setState({
+      hover: true,
+    });
+  };
+
+  handleMouseLeave = () => {
+    this.setState({
+      hover: false,
+    });
+  };
+
+  handleClick = () => {
+    const colors = ['red', 'green', 'gray', 'orange', 'black', 'pink'];
+
+    this.setState({
+      bg: colors[Math.floor(Math.random() * colors.length)],
+    });
+  };
+
+  render() {
+    return (
+      <div className="Animations">
+        <StyledCircle
+          pose={this.state.hover ? 'hover' : 'normal'}
+          onMouseEnter={this.handleMouseEnter}
+          onMouseLeave={this.handleMouseLeave}
+          onClick={this.handleClick}
+          style={{ background: this.state.bg }}
+        >
+          Click me!
+        </StyledCircle>
+      </div>
+    );
+  }
+}
 
 export default index;
